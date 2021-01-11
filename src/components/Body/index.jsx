8ea02530947f00aad9c54def3dd1d502d830da1e
DinobergueViana <dinobergueviana@gmail.com>
2021-01-11 13:29:20 -0300

    adds components folder with Header, Body and Footer components

---
+++ src/components/Body/index.jsx
@@ -0,0 +1,15 @@
+import './Body.css'
+import Card from '../Card';
+
+const Body = () => {
+    return(
+        <section className="App-body">
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+        </section>    
+    )
+}
+
+export default Body;
---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+const Header = () => {
+    return(
+        <footer className="App-footer">
+            <h2>Footer</h2>
+        </footer>
+    )
+}
+
+export default Header;
